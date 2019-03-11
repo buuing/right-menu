99720172c434cbca3c12c4b81b10521c2999a673
ldq <513993984@qq.com>
2019-03-12 00:24:26 +0800

    测试指令

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.querySelector('body').onclick = function () {
-  alert('hello npm')
+export default {
+  // use的时候会触发install
+  install (Vue, options) {
+    /**
+     * vue自定义指令 https://cn.vuejs.org/v2/guide/custom-directive.html
+     */
+    Vue.directive("menu", {
+      // 钩子函数, 第一次绑定时触发
+      bind (el, binding, vnode) {
+        console.log(el, binding, vnode)
+      },
+      // 钩子函数, 解绑时触发
+      unbind (el) {
+        alert('解绑了?')
+      }
+    })
+  }
 }
