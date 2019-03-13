1dabcea976f63ad5d08718ba4e510a049b3f167a
DESKTOP-B7NUKFJ\DELL <17778888138@189.cn>
2019-03-13 17:14:33 +0800

    增加css

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ export default {
       // 钩子函数, 第一次绑定时触发
       bind (el, binding, vnode) {
         // 注册鼠标右击事件
-        console.log({el, binding, vnode})
         el.addEventListener("contextmenu", e => {
           // 阻止默认事件和冒泡
           utils.preventDefault(e)
@@ -15,15 +14,12 @@ export default {
           utils.initMask(el)
           // 初始化菜单栏
           utils.initMenu(binding.value, el, e)
-          // -----------------------
-
-
         })
-        // console.log(document.querySelectorAll('.ldq-menu'))
+        // console.log({el, binding, vnode})
       },
       // 钩子函数, 解绑时触发
       unbind (el) {
-        console.log('解绑了?')
+        // console.log('解绑了?')
       }
     })
   }
--- src/ldq.js
+++ src/ldq.js
@@ -1,13 +1,4 @@
 const ldq = {
-  $: (selector, context = document) => {
-		if (context instanceof NodeList || context instanceof HTMLCollection) {
-			return Array.from(context, node => {
-				return node.querySelector(selector)
-			})
-		}
-		return context.querySelector(selector)
-  },
-
   css: (node, styles) => {
 		node.style.cssText = node.style.cssText ? node.style.cssText += styles : styles
 	},
