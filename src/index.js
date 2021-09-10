09edd414dfffd8419154f1957e17a20645e374ed
ldq <lidongqi1@tal.com>
2021-09-10 15:32:05 +0800

    feat: 增加对 vue3 的支持

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import utils from './utils'
 
+function init (el, binding, vnode) {
+  // 注册鼠标右击事件
+  el.addEventListener('contextmenu', e => {
+    // 阻止默认事件和冒泡
+    utils.preventDefault(e)
+    // 初始化遮罩层
+    utils.initMask(el)
+    // 初始化菜单栏
+    utils.initMenu(binding.value, el, e)
+  })
+}
+
 const install = (Vue, options) => {
   Vue.directive('menu', {
-    bind (el, binding, vnode) {
-      // 注册鼠标右击事件
-      el.addEventListener('contextmenu', e => {
-        // 阻止默认事件和冒泡
-        utils.preventDefault(e)
-        // 初始化遮罩层
-        utils.initMask(el)
-        // 初始化菜单栏
-        utils.initMenu(binding.value, el, e)
-      })
-    }
+    bind: init,
+    beforeMounted: init
   })
 }
 
