16de95fa3e5f11e0bf28c827081a0dc3ac83e537
huanghun <1101774489@qq.com>
2021-09-18 17:48:25 +0800

    兼容mac 暗黑模式、优化theme样式引入逻辑

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import { preventDefault, initMenu } from './utils'
-import { isWin } from './utils/system'
+import { getOperatSystem } from './utils/system'
+import './theme/index.js'
 import './theme/index.less'
 
-if (isWin) {
-  require('./theme/theme-win10.less')
-} else {
+// 添加的主题都按照theme-mac.less theme-win10.less theme-win8.less 的方式来命名
+// [TODO:] 还有一些主题样式暂时缺失，所以这里 try catch 下
+try {
+  require('./theme/theme-' + getOperatSystem().toLowerCase().replace(/is/, '') + '.less')
+} catch (e) {
   require('./theme/theme-mac.less')
 }
+
+// if (isWin) {
+//   require('./theme/theme-win10.less')
+// } else {
+// ./theme/theme-mac.less
+//   require('./theme/theme-mac.less')
+// }
 
 function init (el, binding, options) {
   // 注册鼠标右击事件
