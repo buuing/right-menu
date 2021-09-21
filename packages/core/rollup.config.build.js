e80f95b4ffc2fb7626dd3031a48507d302168ed4
buuing <ldq404@qq.com>
2021-09-21 20:42:26 +0800

    feat: 增加 d.ts 描述文件

--- packages/core/rollup.config.build.js
+++ packages/core/rollup.config.build.js
@@ -6,7 +6,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import babel from 'rollup-plugin-babel'
 import { terser } from 'rollup-plugin-terser'
-import del from 'rollup-plugin-delete'
+// import del from 'rollup-plugin-delete'
 import pkg from './package.json'
 
 export default [
@@ -56,20 +56,4 @@ export default [
       babel({ exclude: 'node_modules/**' }),
     ]
   },
-  // {
-  //   input: "dist/src/index.d.ts",
-  //   output: [
-  //     {
-  //       file: "dist/index.d.ts",
-  //       format: "es"
-  //     }
-  //   ],
-  //   plugins: [
-  //     dts(),
-  //     del({
-  //       targets: 'dist/src',
-  //       hook: 'buildEnd'
-  //     })
-  //   ],
-  // },
 ]
