e0ec1174c1c5f4852933624827c8973023b2cf65
huanghun <1101774489@qq.com>
2021-10-11 11:20:18 +0800

    骨架屏mac 主题色

--- packages/core/src/theme/index.ts
+++ packages/core/src/theme/index.ts
@@ -1,5 +1,5 @@
 
-import { systemDarkTheme, systemLightTheme } from '../config/index'
+import { systemDarkTheme, systemLightTheme,  skeletonColorLight, skeletonColorDark } from '../config/index'
 import { getOperatSystem } from '../utils/system'
 
 import './index.less'
@@ -8,12 +8,19 @@ import './theme-win10.less'
 
 const systemTheme = window.matchMedia('(prefers-color-scheme: dark)')
 
+let skeletonColor: Array<string>
+
 const watchSystemThemeChange = () => {
   const rootElement = document.querySelector(':root')
   rootElement && rootElement.setAttribute('style', systemTheme.matches ? systemDarkTheme : systemLightTheme)
+  skeletonColor = systemTheme.matches ? skeletonColorDark : skeletonColorLight
 }
 // 先判断下当前系统的主题
 watchSystemThemeChange()
 
 systemTheme.addEventListener('change', watchSystemThemeChange)
+
+export {
+  skeletonColor
+}
 
--- packages/core/src/utils/skeleton.ts
+++ packages/core/src/utils/skeleton.ts
@@ -1,10 +1,12 @@
+import { skeletonColor } from '../theme/index'
+
 export const calculate = (element:HTMLElement) => {
   const { lineHeight, padding } = window.getComputedStyle(element)
   const [ paddingTop ] = padding.split(' ')
   const height = parseFloat(lineHeight)  + parseFloat(paddingTop) * 2
   return `
     height: ${height}px;
-    background-image: linear-gradient(-45deg, #f5f5f5 40%, #fff 55%, #f5f5f5 63%);
+    background-image: linear-gradient(-45deg, ${skeletonColor[0]} 40%, ${skeletonColor[1]} 55%, ${skeletonColor[0]} 63%);
     background-size: 400% ${lineHeight};
     background-repeat: no-repeat;
     position: relative;
